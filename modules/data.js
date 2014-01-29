62dc87a8e4bd3040169981ebefbadaa3d2a44cfc
Artem Kholodenko <artemk@gmail.com>
2014-01-28 22:23:59 -0800

    moved location data into separate module and made marker creation more generic

---
+++ modules/data.js
@@ -0,0 +1,16 @@
+/** DATA module */
+define(function () {
+    return [
+    	{
+    		id: 1,
+		title: 'Washington Park',
+  		lat: 37.5817245,
+  		lng: -122.3426732
+	},
+	{
+		id: 2,
+		title: 'Central Park',
+  		lat: 37.5607982,
+  		lng: -122.3234643
+	}]
+});
--- modules/map.js
+++ modules/map.js
@@ -1,5 +1,5 @@
 /** MAP module */
-define(function () {
+define(["./data"], function(locationData) {
     return {
     	geocode: function (address) {
     		// use geocoder to find longitute/latitude location of current address
@@ -11,59 +11,60 @@ define(function () {
   	},
   	 // callback to geocode lookup for lat/long of location
   	render: function (results, status) {
-	    var map;
+	    	var map;
 
-	    // upon successful address to lat/long resolution
-	    if (status == google.maps.GeocoderStatus.OK) {      
-	      // basic map options
-	      var mapOptions = {
-	          center: results[0].geometry.location, // center map on resolved geolocation
-	          mapTypeId: google.maps.MapTypeId.ROADMAP  // default map is classic map
-	      }
+	    	// upon successful address to lat/long resolution
+	    	if (status == google.maps.GeocoderStatus.OK) {      
+	      	// basic map options
+	      	var mapOptions = {
+	        	center: results[0].geometry.location, // center map on resolved geolocation
+	          	mapTypeId: google.maps.MapTypeId.ROADMAP  // default map is classic map
+	      	}
 
-	      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	      	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-	      // zoom map to be within radius of circle (zoom based on distance, not present number)
-	      var circle = new google.maps.Circle({radius: 16000, center: results[0].geometry.location}); 
-	      map.fitBounds(circle.getBounds()); 
+	      	// zoom map to be within radius of circle (zoom based on distance, not present number)
+	      	var circle = new google.maps.Circle({radius: 16000, center: results[0].geometry.location}); 
+	      	map.fitBounds(circle.getBounds()); 
 
-	      // draw marker in center of map
-	      var marker = new google.maps.Marker({
-	          map: map,
-	          title: 'You\'re Here',
-	          position: results[0].geometry.location,
-	          animation: google.maps.Animation.DROP
-	      });     
+	      	// draw marker in center of map
+	      	var marker = new google.maps.Marker({
+	          	map: map,
+	          	title: 'You\'re Here',
+	          	position: results[0].geometry.location,
+	          	animation: google.maps.Animation.DROP
+	      	});     
 
-	      // add Burlingame playground
-	      var washingtonParkMarker = new google.maps.Marker({
-	          map: map,
-	          title: 'Washington Park',
-	          position: new google.maps.LatLng(37.5817245,-122.3426732),
-	          animation: google.maps.Animation.DROP,
-	          icon: 'images/icon_playground_32.png',
-	          id: 57 // unique identifier for location
-	      });     
+	      	var infoWindow = new google.maps.InfoWindow({
+	            	content: 'demo',
+	            	maxWidth: 500
+	      	});
 
-	      var infoWindow = new google.maps.InfoWindow({
-	            content: 'demo',
-	            maxWidth: 500
-	        });
+		locationData.forEach(function(locationObject) {	      	
+			var locationMarker = new google.maps.Marker({
+		          	map: map,
+		          	title: locationObject.title,
+		          	position: new google.maps.LatLng(locationObject.lat, locationObject.lng),
+		          	animation: google.maps.Animation.DROP,
+		          	icon: 'images/icon_playground_32.png',
+		          	id: locationObject.id // unique identifier for location
+		      	});   
 
-	      google.maps.event.addListener(marker, 'click', function() {
-	        infoWindow.setContent('You are here');
-	        infoWindow.setPosition(marker.getPosition());
-	        infoWindow.open(map,marker);
-	      });     
+		      	google.maps.event.addListener(locationMarker, 'click', function() {
+		        	console.log(locationMarker.get('title') + ' (' + locationMarker.get('id') + ')');
 
-	      google.maps.event.addListener(washingtonParkMarker, 'click', function() {
-	        console.log(washingtonParkMarker.get('title') + ' (' + washingtonParkMarker.get('id') + ')');
+		        	infoWindow.setContent(locationMarker.get('title'));
+		        	infoWindow.setPosition(locationMarker.getPosition());
+		        	infoWindow.open(map, locationMarker);
+		      	});   
+	      	});
 
-	        infoWindow.setContent(washingtonParkMarker.get('title'));
-	        infoWindow.setPosition(washingtonParkMarker.getPosition());
-	        infoWindow.open(map,washingtonParkMarker);
-	      });     
+	      	google.maps.event.addListener(marker, 'click', function() {
+		        infoWindow.setContent('You are here');
+		        infoWindow.setPosition(marker.getPosition());
+	        	infoWindow.open(map,marker);
+		});       
 	    }
-	  }
+	}
     }
 });
