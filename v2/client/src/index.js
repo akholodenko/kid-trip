609e59bbaa5668769309015124c84db05777bf15
Artem Kholodenko <artemk@gmail.com>
2019-01-27 22:38:28 -0800

    use package for font

--- v2/client/src/index.js
+++ v2/client/src/index.js
@@ -8,6 +8,7 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from 'react-router-dom'
+import 'typeface-roboto';
 
 const client = new ApolloClient({
 	uri: 'http://localhost:4000',
