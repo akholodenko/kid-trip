f0128678eff1dfa01b652620316a46e787e1ec60
Artem Kholodenko <artemk@gmail.com>
2020-01-01 21:33:30 -0800

    setup local env variables and convert for use

--- v2/server/config/sequelize.js
+++ v2/server/config/sequelize.js
@@ -1,6 +1,15 @@
-import Sequelize from "sequelize";
+import dotenv from 'dotenv'
+import Sequelize from "sequelize"
 
-export default new Sequelize('kidtrip', 'root', null, {
-	host: 'localhost',
-	dialect: 'postgres'
-});
+const result = dotenv.config({ path: 'config.env' })
+
+if (result.error) {
+	throw result.error
+}
+
+const { DB_SCHEMA, DB_USER, DB_PASSWORD, DB_HOST } = result.parsed
+
+export default new Sequelize(DB_SCHEMA, DB_USER, DB_PASSWORD, {
+	host: DB_HOST,
+	dialect: 'postgres',
+})
