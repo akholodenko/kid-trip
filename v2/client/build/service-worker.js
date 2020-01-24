0de9c3bcbb52042a0a9b51b35457468c84d516e6
Artem Kholodenko <artemk@gmail.com>
2020-01-23 23:59:53 -0800

    no message

--- v2/client/build/service-worker.js
+++ v2/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3184c85da9fd563ce93fad8b0b1de3bd.js"
+  "/precache-manifest.7d4db6be7f45bdf48a8decab68e34425.js"
 );
 
 self.addEventListener('message', (event) => {
