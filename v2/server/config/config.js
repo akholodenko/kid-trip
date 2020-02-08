52e9336e0269da2fb78d2e3dd17646679c332deb
Artem Kholodenko <artemk@gmail.com>
2020-02-08 14:34:00 -0800

    sequelize migration environment setip

---
+++ v2/server/config/config.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv')
+
+const result = dotenv.config({ path: 'config.env' })
+
+if (result.error) {
+  throw result.error
+}
+
+const { DB_SCHEMA, DB_USER, DB_PASSWORD, DB_HOST } = result.parsed
+
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": DB_SCHEMA,
+    "host": DB_HOST,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_SCHEMA,
+    "host": DB_HOST,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  }
+}
