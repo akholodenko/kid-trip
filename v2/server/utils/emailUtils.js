0a234ca2baf48281cd313cee7e6b0fb675b60d16
Artem Kholodenko <artemk@gmail.com>
2020-02-09 12:18:57 -0800

    convert to template support for welcome email

--- v2/server/utils/emailUtils.js
+++ v2/server/utils/emailUtils.js
@@ -9,16 +9,28 @@ if (result.error) {
 const { SENDGRID_KEY } = result.parsed
 sgMail.setApiKey(SENDGRID_KEY);
 
+const WELCOME_EMAIL_TEMPLATE_ID = 'd-2d4384187beb44778042eab3b873657b'
+
 export const sendWelcomeEmail = (user) => {
 	sendEmail({
-		to: user.email,
-		subject: 'Welcome to KidTrip!',
-		text: `Hi ${user.firstName}`,
-		html: `<strong>Hi ${user.firstName}!</strong><p> Welcome back to childhood.</p>`,
+		template_id: WELCOME_EMAIL_TEMPLATE_ID,
+		personalizations: [
+			{
+				to: {
+					name: `${user.firstName} ${user.lastName}`,
+					email: user.email
+				},
+				dynamic_template_data: {
+					subject: `Welcome to KidTrip, ${user.firstName}!`,
+					firstName: user.firstName,
+					lastName: user.lastName,
+				}
+			}
+		]
 	})
 }
 
 const sendEmail = (message) => {
-	message.from = 'no-reply@mykidtrip.com'
+	message.from = { name: 'Artem K.', email: 'artem@mykidtrip.com'}
 	sgMail.send(message);
 }
