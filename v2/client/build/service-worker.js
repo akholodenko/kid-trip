f08349f15a04f3d1761a3496145a83d37a8c6f3c
Artem Kholodenko <artemk@gmail.com>
2020-02-22 17:56:10 -0800

    latest release

--- v2/client/build/service-worker.js
+++ v2/client/build/service-worker.js
@@ -12,30 +12,30 @@
  */
 
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+)
 
-importScripts("/precache-manifest.110acccc3b9e9a5bed0f633adc38fbe7.js");
+importScripts('/precache-manifest.19780472a57985687de35cc6aa85a283.js')
 
-self.addEventListener("message", event => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
   }
-});
+})
 
-workbox.core.clientsClaim();
+workbox.core.clientsClaim()
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
 workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL("/index.html"),
+  workbox.precaching.getCacheKeyForURL('/index.html'),
   {
     blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/]
   }
-);
+)
