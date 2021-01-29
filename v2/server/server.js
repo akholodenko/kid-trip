8396c0f0ef4bd73010ada9a06d72c16e4572f98d
Artem Kholodenko <artemk@gmail.com>
2021-01-28 18:38:44 -0800

    p3

--- v2/server/server.js
+++ v2/server/server.js
@@ -11,7 +11,7 @@ const schema = makeExecutableSchema({
 
 const server = new ApolloServer({
   cors: {
-    origin: '*', // <- allow request from all domains
+    origin: '*',
     credentials: true
   },
   schema,
