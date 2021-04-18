ca8d4faa20efb40b0d93b9be2b9f2e0fc6ff65d5
Artem Kholodenko <artemk@gmail.com>
2021-04-18 12:28:38 -0700

    prod build

--- v2/server/resolvers/zipcode.js
+++ v2/server/resolvers/zipcode.js
@@ -1,12 +1,12 @@
-import Zipcode from "../models/zipcode"
+import Zipcode from '../models/zipcode'
 
 async function getZipCode(zipcode) {
-	const response = await Zipcode.findOne({
-		where: { zip: zipcode },
-		attributes: ['zip', 'lat', 'lng'],
-	})
+  const response = await Zipcode.findOne({
+    where: { zip: zipcode },
+    attributes: ['zip', 'lat', 'lng']
+  })
 
-	return { lat: response.lat, lng: response.lng }
+  return { lat: response.lat, lng: response.lng }
 }
 
-module.exports.getZipCode = getZipCode;
+module.exports.getZipCode = getZipCode
